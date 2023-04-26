4ed434a78e4a5fabb6704fb7e9af29d433875a6e
Mikayla <user@users-MBP.attlocal.net>
2023-04-26 15:48:10 -0400

    created quiz buttons & styled, wrote question divs

---
+++ Assets/script.js
@@ -0,0 +1,44 @@
+let questions= [
+    {
+        question: "Which of the following is an element for the box model?",
+        answers: [
+            { text: "Margin", correct: true},
+            { text: "Absolutes", correct: false},
+            { text: "Psuedo-class", correct: false},
+            { text: "Flex", correct: false},
+        ]
+    },
+    {
+        question: "What is an array?",
+        answers: [
+            { text: "A list of separated operators", correct: false},
+            { text: "A list of separated values", correct: true},
+            { text: "A list of console.logs", correct: false},
+            { text: "A grocery list", correct: false},
+        ]
+    },
+    {
+        question: "What is a Boolean value?",
+        answers: [
+            { text: "A value with a string", correct: false},
+            { text: "A value with user input", correct: false},
+            { text: "A true or false value", correct: true},
+            { text: "A if, else condition", correct: false},
+        ]
+    },
+    {
+        question: "Which of the following defines what DOM stands for?",
+        answers: [
+            { text: "Document Override Model", correct: false},
+            { text: "Days on Market", correct: false},
+            { text: "Document Object Maker", correct: false},
+            { text: "Document Object Model", correct: true},
+        ]
+    }
+];
+
+var questionElement = document.getElementById("question");
+var answerButtonElement = document.getElementById("answer-buttons");
+let 
+
+
